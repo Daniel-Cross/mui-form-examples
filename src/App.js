1bfc3714d2352d8a0ec02ec8fc4ca8089b722da6
Daniel-Cross <crossy1686@gmail.com>
2020-10-27 11:42:34 +0100

    added form examples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Box, Container, Paper, TextField } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={3}>
+      <Container maxWidth="md">
+        <Paper>
+          <form action="submit">
+            <Box m={5} p={2}>
+              <h2>Add Venue Form</h2>
+            </Box>
+            <Box m={5}>
+              <Grid item xs={12}>
+                <TextField label="Name" variant="outlined" fullWidth />
+              </Grid>
+            </Box>
+
+            <Box m={5}>
+              <Grid container xs={12} spacing={3}>
+                <Grid item xs={4} spacing={3}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Box m={5}>
+              <Grid container xs={12} spacing={3}>
+                <Grid item xs={6} spacing={3}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Box m={5}>
+              <Grid container xs={12} spacing={3}>
+                <Grid item xs={3} spacing={3}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField label="Name" variant="outlined" fullWidth />
+                </Grid>
+              </Grid>
+            </Box>
+          </form>
+        </Paper>
+      </Container>
+    </Grid>
   );
 }
 
